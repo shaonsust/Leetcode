0e97a34812d94d8cb0f0986e805e1e4d49b1860f
shaonsust <shaon63@gmail.com>
2022-12-05 00:58:36 +0600

    leetcode problem no 2490 solved

--- 2490_circular_sentence.py
+++ 2490_circular_sentence.py
@@ -1,12 +1,18 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
-        pass
+        if sentence[0] != sentence[-1]:
+            return False
+
+        str_list = sentence.split()
+        for i in range(1, len(str_list)):
+            if str_list[i][0] != str_list[i-1][-1]:
+                return False        
+
+        return True
 
 
 if __name__ == '__main__':
     for i in range(int(input())):
         sentence = input()
-        if "S" == "s":
-            print("true")
-        else:
-            print("false")
+        obj = Solution()
+        print(obj.isCircularSentence(sentence=sentence))
