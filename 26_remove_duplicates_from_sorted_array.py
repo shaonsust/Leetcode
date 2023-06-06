3d8718ad8a5e2d9c182878281929dcd0dad4688c
s m shaon <shaon63@gmail.com>
2023-06-06 17:49:47 +0600

    remove duplicates from sorted array

---
+++ 26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def remove_duplicates(self, nums: List[int]) -> int:
+        k: int = 0
+
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] = 200
+                k += 1
+
+        nums.sort()
+        return len(nums) - k
+    
+    def remove_duplicates_two_pointer(self, nums: List[int]) -> int:
+        k: int = 1
+        for i in range(1, len(nums)):
+            if nums[i-1] != nums[i]:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
+    
+    def remove_duplicates_set(self, nums: List[int]) -> int:
+        nums[:] = list(set(nums))
+        
+        return len(nums)
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        nums = [int(x) for x in input().split()]
+
+        obj = Solution()
+        print(obj.remove_duplicates_set(nums=nums))
